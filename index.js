b791abe5ef14726e9693963be6c1b50c55a9dd9e
shadin710 <shadin710@gmail.com>
2020-08-27 02:08:30 +0600

    Added login and register route

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+
+//initialization 
 const path  = require('path');
 const express = require('express');
 const app = express();
+const log = require('./routes/login');
+const reg = require('./routes/register');
+const forget = require('./routes/forget');
+const morgan = require('morgan');
+const cors = require('cors');
 
-
+//setting the port
 const port = process.env.PORT||3000
 app.use(express.static(path.join(__dirname,'views')));
 
-
+// setting up the view engine
 app.set('view engine','ejs');
 app.set ('views','./views');
 
-app.get('/login',(req,res)=>{
-    res.render('login');
-});
+app.use(morgan('dev'));
+app.use(cors());
+
+//setting up the routes
+app.use('/',log);
 app.get('/register',(req,res)=>{
     res.render('register');
 })
---
+++ routes/login.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const express = require('express');
+const path = require('path');
+const bodyParser =  require('body-parser');
+const bcrypt = require('bcryptjs');
+const {check, validationResult}  =require('express-validator');
+const { resolveMx } = require('dns');
+
+
+router.use(express.static(path.join(__dirname,'./../views')));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:true})); 
+
+
+router.get('/',(req,res)=>{
+    res.render('login');
+});
+router.get('/logout',(req,res)=>{
+    res.send("logged out");
+});
+router.get('/google',(req,res)=>{
+    res.send('google+ login');
+});
+module.exports = router; 
